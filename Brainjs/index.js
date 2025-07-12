fb7380d4274cf3f3c8b2fcfe6afec3870d5ff5f1
matihullahIT <matihurrehman.official@gmail.com>
2025-07-12 17:09:06 +0500

    update 12 files and delete 2865 files

--- Brainjs/index.js
+++ Brainjs/index.js
@@ -1,9 +1,13 @@
 const brain = require("brain.js");
-const net = new brain.NeuralNetwork();
-const fs = require("fs");
-const Data = fs.readFileSync("./tm-my-image-model/salaries.csv", "utf8");
 
-function FetchData() {
-  console.log(Data);
-}
-FetchData();
+const net = new brain.NeuralNetwork();
+
+net.train([
+  { input: [0, 0], output: [0] },
+  { input: [0, 1], output: [1] },
+  { input: [1, 0], output: [1] },
+  { input: [1, 1], output: [0] },
+]);
+
+const output = net.run([1, 0]);
+console.log(output);
